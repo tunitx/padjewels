073089e9a58e02237097fdff3cc91f3d8d9f6789
tanish-goloakit <tanish@golokait.com>
2024-05-06 17:51:34 +0530

    fixed about sex

--- client/client/src/pages/other/About.js
+++ client/client/src/pages/other/About.js
@@ -1,14 +1,8 @@
-import { Fragment } from "react"; 
-import { useLocation } from "react-router-dom"; 
+import { Fragment } from "react";
+import { useLocation } from "react-router-dom";
 import SEO from "../../components/seo";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
-import SectionTitleWithText from "../../components/section-title/SectionTitleWithText";
-import BannerOne from "../../wrappers/banner/BannerOne";
-import TextGridOne from "../../wrappers/text-grid/TextGridOne";
-import FunFactOne from "../../wrappers/fun-fact/FunFactOne";
-import TeamMemberOne from "../../wrappers/team-member/TeamMemberOne";
-import BrandLogoSliderOne from "../../wrappers/brand-logo/BrandLogoSliderOne";
 
 const About = () => {
   let { pathname } = useLocation();
@@ -18,39 +12,71 @@ const About = () => {
       <SEO
         titleTemplate="About us"
         description="About page of flone react minimalist eCommerce template."
-      /> 
+      />
       <LayoutOne headerTop="visible">
         {/* breadcrumb */}
-        <Breadcrumb 
+        <Breadcrumb
           pages={[
-            {label: "Home", path: process.env.PUBLIC_URL + "/" },
-            {label: "About us", path: process.env.PUBLIC_URL + pathname }
-          ]} 
+            { label: "Home", path: process.env.PUBLIC_URL + "/" },
+            { label: "About us", path: process.env.PUBLIC_URL + pathname }
+          ]}
         />
 
-        {/* section title with text */}
-        <SectionTitleWithText spaceTopClass="pt-100" spaceBottomClass="pb-95" />
+        <div className="w-full h-full flex flex-col items-center justify-center gap-y-10 my-16">
+          {/* about section */}
+          <div className="w-[80%] h-auto flex flex-col justify-center items-center space-y-6 ">
+            <h1 className="text-3xl underline  hover:cursor-pointer ">
+              About Us
 
-        {/* banner */}
-        <BannerOne spaceBottomClass="pb-70" />
+            </h1>
+            <div className="w-full h-full  bg-gray-100 rounded-lg flex flex-col justify-center items-center py-6 px-4">
+              <div className="w-[100%] h-[100%] ">
+              <p className="text-lg  hover:cursor-pointer">
+              We exist to craft high quality, exquisite designs, exceptional management and extra ordinary service. We Pad Jewels epitomizes sophistications, elegance and innovation in every piece we create. Our dedicated design and development team enable us cater to current trend and hence being available to our customers with unique and classic designs.
 
-        {/* text grid */}
-        <TextGridOne spaceBottomClass="pb-70" />
+            </p>
+              </div>
+            </div>
+           
+          </div>
+          {/* our mission */}
+          <div className="w-[80%] h-auto flex flex-col justify-center items-center  space-y-6"  >
+            <h1 className="text-3xl underline hover:cursor-pointer">
+              Our Mission
 
-        {/* fun fact */}
-        <FunFactOne
-          spaceTopClass="pt-100"
-          spaceBottomClass="pb-70"
-          bgClass="bg-gray-3"
-        />
+            </h1>
+            <div className="w-full h-full  bg-gray-100 rounded-lg flex flex-col justify-center items-center py-6 px-4">
+              <div className="w-[100%] h-[100%] ">
+              
+                <p className="text-lg hover:cursor-pointer">
+                  Our website dedicated to imitation jewellery aims to redefine elegance and affordability. Our mission is to provide access to high-quality, meticulously crafted pieces that capture the essence of luxury without the hefty price tag. We believe that everyone deserves to adorn themselves with stunning accessories that reflect their unique style and personality. From classic elegance to contemporary chic, our collection is curated with care to ensure that each piece becomes a cherished addition to your wardrobe. Join us on our mission to make luxury accessible to all, one exquisite piece at a time.
 
-        {/* team member */}
-        <TeamMemberOne spaceTopClass="pt-95" spaceBottomClass="pb-70" />
+                </p>
+              </div>
 
-        {/* brand logo slider */}
-        <BrandLogoSliderOne spaceBottomClass="pb-70" />
+
+            </div>
+          </div>
+          {/* our approach */}
+          <div className="w-[80%] h-auto flex flex-col justify-center items-center  space-y-6">
+            <h1 className="text-3xl underline hover:cursor-pointer">
+              Our Approach
+            </h1>
+            <div className="w-full h-full  bg-gray-100 rounded-lg flex flex-col justify-center items-center py-6 px-4">
+              <div>
+              <p className="text-lg hover:cursor-pointer">
+              At our imitation jewellery website, our approach is rooted in three core principles: quality, variety, and customer service with satisfaction. We prioritize quality by meticulously selecting materials and artisans who craft each piece with precision and care, ensuring that every item exudes a sense of luxury and durability. Our commitment to variety means offering a diverse range of designs, from classic to contemporary, ensuring that there's something to suit every taste and occasion. But above all, we prioritize customer satisfaction, offering a seamless shopping experience from browsing to purchase and beyond. We provide detailed product information, responsive customer service, and hassle-free returns, ensuring that our customers feel confident and delighted with their jewellery choices.
+            </p>
+              </div>
+            </div>
+           
+          </div>
+        </div>
+
       </LayoutOne>
+
     </Fragment>
+
   );
 };
 
